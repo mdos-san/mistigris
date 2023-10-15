2f8e8562485b1fdb63ba488d296fd9ea2a73833b
mdos-san <mdossan@protonmail.com>
2023-10-16 00:33:44 +0200

    Create `FamilyLogic`

--- src/Models/index.ts
+++ src/Models/index.ts
@@ -1,13 +1,16 @@
 import CardSelectorFactory from "./CardSelector";
+import FamilyLogicFactory from "./FamilyLogic";
 import { FirebaseServiceFactory } from "./firebase";
 
 function ModelsFactory() {
   const CardSelector = CardSelectorFactory();
   const FirebaseService = FirebaseServiceFactory();
+  const FamilyLogic = FamilyLogicFactory(FirebaseService);
 
   return {
     CardSelector,
     FirebaseService,
+    FamilyLogic,
   };
 }
 
